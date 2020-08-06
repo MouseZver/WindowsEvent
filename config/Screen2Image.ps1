cf0ae6692564bce4be9cfc8876c29b1aa8aa95c8
Mouse <mouse-zver@xaker.ru>
2020-08-06 10:25:58 +0300

    save binary code

--- config/Screen2Image.ps1
+++
@@ -1,8 +0,0 @@
-[reflection.assembly]::LoadWithPartialName( "System.Drawing" ) > $null;
-$Bitmap = New-Object System.Drawing.Bitmap {{width}}, {{height}};
-$Size = New-Object System.Drawing.Size {{width}}, {{height}};
-$FromImage = [System.Drawing.Graphics]::FromImage( $Bitmap );
-$FromImage.CopyFromScreen( {{x}}, {{y}}, 0, 0, $size );
-$FromImage.Dispose();
-$Bitmap.Save( "{{file}}", [System.Drawing.Imaging.ImageFormat]::png );
-$Bitmap.Dispose();
--- config/cursorPosition.ps1
+++
@@ -1,2 +0,0 @@
-Add-Type -as System.Windows.Forms;
-[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point( {{x}}, {{y}} );
